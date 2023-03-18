532d2c26dc610313982f4a62bf6e442c309e2a07
Richard Poroszlay <richardporoszlay@gmail.com>
2023-03-18 15:30:55 +0100

    Add documentation to happy_new_year homework

--- 05/hazik/happy_new_year.py
+++ 05/hazik/happy_new_year.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 
+"""
+Ebben a programban úgy fogjuk kiírni a 2023-at, hogy nem használunk számjegyeket a kódban.
+"""
+
+
 def new_year():
+    """
+    Egy lista segítségével hajtom végre a 2023 kiíratását, melyben különböző hosszúságú stringek vannak.
+    Ezeknek a hosszúságait felhasználva egy result változóba kiírom a kért dátumot.
+    """
     components = ["xx", "", "xx", "xxx"]
     result = ""
     for component in components:
